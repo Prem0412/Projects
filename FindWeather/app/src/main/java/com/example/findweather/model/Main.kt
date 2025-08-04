1eb9415e455dde72a58cf9fd0ef3afe8fc79acca
Prem0412 <Premsantoshpardeshi@gmail.com>
2025-08-04 11:57:28 +0530

    not needed

--- FindWeather/app/src/main/java/com/example/findweather/model/Main.kt
+++
@@ -1,8 +0,0 @@
-package com.example.findweather.model
-
-data class Main(val temp: Double
-)
-
-data class Weather(
-    val description: String
-)
--- FindWeather/app/src/main/java/com/example/findweather/model/WeatherResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.findweather.model
-
-data class WeatherResponse(
-    val main: Main,
-    val weather: List<Weather>
-)
-
